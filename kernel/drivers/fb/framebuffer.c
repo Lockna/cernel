cd6b22111675652a3fb2f254aaa001598deb4e03
Raphael O <raphael.ob@protonmail.com>
2021-02-12 12:46:49 +0100

    Refactor doc comments

--- kernel/drivers/fb/framebuffer.c
+++ kernel/drivers/fb/framebuffer.c
@@ -9,15 +9,6 @@ uint16_t height = 0;
 uint16_t bpp = 0;
 uint16_t pitch = 0;
 
-/**
- *	@brief Sets the global variables to their corrosponding value
- *	@param fb_adress
- *	@param framebuffer_width
- *	@param framebuffer_height
- *	@param framebuffer_bpp
- *	@param framebuffer_pitch
- *	@return 0 at success, since nothing is done, that could fail, it just returns 0
- */
 int load_fb_driver(uint64_t *fb_adress, 
 				uint16_t framebuffer_width,
 				uint16_t framebuffer_height, 
@@ -32,12 +23,6 @@ int load_fb_driver(uint64_t *fb_adress,
 	return 0;
 }
 
-
-/**
- *	@brief Prints a single character to the pixelframebuffer
- *	@param c char to be printed
- *	@return void
- */
 void putc(char c) 
 {	
 	int index = 0;
@@ -87,11 +72,6 @@ void putc(char c)
 	cursor_loc -= pitch*16;
 }
 
-/**
- *	@brief Writes a string to the pixelframebuffer using putc
- *	@param str string to be printed
- *	@return void
- */
 void puts(char *str) {
 
 	int index = 0;
--- kernel/include/cernel/drivers/fb/framebuffer.h
+++ kernel/include/cernel/drivers/fb/framebuffer.h
@@ -3,18 +3,33 @@
 
 #include <stdint.h>
 
-#define LINES 25
-#define COLUMNS_PER_LINE 80
-#define BYTES_PER_ELEMENT 2
-#define LINESIZE = BYTES_PER_ELEMENT * COLUMNS_PER_LINE
-#define SCREENSIZE LINES * LINESIZE
-
+/**
+ *	@brief Sets the global variables to their corrosponding value
+ *	@param fb_adress
+ *	@param framebuffer_width
+ *	@param framebuffer_height
+ *	@param framebuffer_bpp
+ *	@param framebuffer_pitch
+ *	@return 0 at success, since nothing is done, that could fail, it just returns 0
+ */
 int load_fb_driver(uint64_t *fb_adress, 
                 uint16_t framebuffer_width,
                 uint16_t framebuffer_height, 
                 uint16_t framebuffer_bpp,
                 uint16_t framebuffer_pitch);
+
+/**
+ *	@brief Prints a single character to the pixelframebuffer
+ *	@param c char to be printed
+ *	@return void
+ */
 void putc(char c);
+
+/**
+ *	@brief Writes a string to the pixelframebuffer using putc
+ *	@param str string to be printed
+ *	@return void
+ */
 void puts(char *str);
 
 #endif
