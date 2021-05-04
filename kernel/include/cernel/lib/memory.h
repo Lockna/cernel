a0012c39705e0de657c68844ed7d6fcfbf0bae34
0x6D70 <pfeifenberger.markus2003@gmail.com>
2021-05-04 20:21:04 +0200

    Add memset, memset64

--- kernel/include/cernel/lib/memory.h
+++ kernel/include/cernel/lib/memory.h
@@ -3,3 +3,6 @@
 #include <stivale/stivale.h>
 
 size_t get_memory_size(struct stivale_mmap_entry *memory_map_addr, uint32_t count);
+
+void *memset(uint8_t *ptr, uint8_t value, size_t count);
+void *memset64(uint64_t *ptr, uint64_t value, size_t count);
--- kernel/lib/memory.c
+++ kernel/lib/memory.c
@@ -13,3 +13,20 @@ size_t get_memory_size(struct stivale_mmap_entry *memory_map_addr, uint32_t coun
     return memory_size;
 
 }
+
+void *memset(uint8_t *ptr, uint8_t value, size_t count) {
+	for (size_t i = 0; i < count; i++) {
+		ptr[i] = value;
+	}
+
+	return ptr;
+}
+
+void *memset64(uint64_t *ptr, uint64_t value, size_t count) {
+	for (size_t i = 0; i < count; i++) {
+		ptr[i] = value;
+	}
+
+	return ptr;
+}
+
