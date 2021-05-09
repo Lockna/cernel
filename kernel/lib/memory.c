22d268929fc230f1a7f8768d3780fadf6bd87237
Lockna <raphael.ob@protonmail.com>
2021-05-09 14:12:07 +0200

    Refactor code

--- kernel/lib/memory.c
+++ kernel/lib/memory.c
@@ -7,14 +7,13 @@ size_t get_memory_size(struct stivale_mmap_entry *memory_map_addr, uint32_t coun
 
     for (uint32_t i = 0; i < count; i++) {
         memory_size += memory_map_addr[i].length;
-
     }
 
     return memory_size;
-
 }
 
 void *memset(uint8_t *ptr, uint8_t value, size_t count) {
+
 	for (size_t i = 0; i < count; i++) {
 		ptr[i] = value;
 	}
@@ -23,10 +22,10 @@ void *memset(uint8_t *ptr, uint8_t value, size_t count) {
 }
 
 void *memset64(uint64_t *ptr, uint64_t value, size_t count) {
+
 	for (size_t i = 0; i < count; i++) {
 		ptr[i] = value;
 	}
 
 	return ptr;
 }
-
