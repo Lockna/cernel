81c97a6eb686a8a5134d27db5ed6d4cb00958379
Lockna <raphael.ob@protonmail.com>
2021-05-13 19:47:53 +0200

    Add doc comments for apic

--- kernel/include/cernel/interrupt/apic.h
+++ kernel/include/cernel/interrupt/apic.h
@@ -1,1 +1,9 @@
 #pragma once
+
+/**
+ *	@file apic.h
+ *	@brief This file will hold the function prototypes for the apic
+ *
+ *  These functions are essential for the apic and IRQ handling, additionally these are important
+ *  handling keyboard interrupts given by ps/2 keyboards.
+ */
--- kernel/interrupt/apic.c
+++ kernel/interrupt/apic.c
@@ -1,1 +1,9 @@
+/**
+ *	@file apic.c
+ *	@brief IRQ Handling via the apic
+ *
+ *  IRQ's are getting handled here by the apic, the advanced programmable interrupt controller
+ *  Later on there are lapics that will handle the IRQ's of the core the lapic belongs to
+ */
+
 #include <cernel/interrupt/apic.h>
