2df76571bdc9a6c9b289c04f07c18e7185f8606e
0x6D70 <pfeifenberger.markus2003@gmail.com>
2021-06-07 08:20:55 +0200

    Revert "Bootstrap code for apic"
    This reverts commit 9d7ac37f37b5a9d452e4c7e1527cc93d1a608263.

--- kernel/include/cernel/interrupt/apic.h
+++ kernel/include/cernel/interrupt/apic.h
@@ -7,9 +7,3 @@
  *  These functions are essential for the apic and IRQ handling, additionally these are important
  *  handling keyboard interrupts given by ps/2 keyboards.
  */
-
-void init_apic();
-
-void lapic_enable();
-
-void lapic_write(uint32_t reg, uint32_t data);
