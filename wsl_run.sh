451fd5fc34aeb7210afec88cf97c3ee70b8ee6f5
Raphael O <raphael.ob@protonmail.com>
2021-09-16 10:04:59 +0200

    Remove unused script
    After I adjusted the makefile to differ between windows, wsl and linux I forgot to delete this extra script we used before to start our qemu environment in wsl.

--- wsl_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ "$1" == "debug" ]; then
-	qemu-system-x86_64.exe -m 1G -debugcon stdio -drive file=./cernel.hdd,format=raw -s -S
-else
-	qemu-system-x86_64.exe -m 1G -debugcon stdio -drive file=./cernel.hdd,format=raw -no-reboot
-fi
