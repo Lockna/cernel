777ffcf0cd19bdd61d235a7db47ed700391895db
0x6D70 <pfeifenberger.markus2003@gmail.com>
2021-11-11 11:39:21 +0100

    implement ubsan type mismatch

--- kernel/misc/ubsan.c
+++ kernel/misc/ubsan.c
@@ -2,6 +2,13 @@
 #include <cernel/lib/print.h>
 #include <cernel/interrupt/panic.h>
 
+const char *type_check_kinds[] = {
+    "load of", "store to", "reference binding to", "member access within",
+    "member call on", "constructor call on", "downcast of", "downcast of",
+    "upcast of", "cast to virtual base of", "_Nonnull binding to",
+    "dynamic operation on"
+};
+
 struct source_location {
     char *filename;
     uint32_t line;
@@ -39,7 +46,20 @@ void __ubsan_handle_shift_out_of_bounds(struct shift_out_ouf_bounds_data *data, 
 }
 
 void __ubsan_handle_type_mismatch_v1(struct type_mismatch_data *data, void *pointer) {
+    // TODO: handle other options
+    
+    uint64_t alignment = (uint64_t)1 << data->log_alignment;
+
+    if ((uint64_t)pointer & (alignment - 1)) {
+        kprintf("alignment issue\n");
+
+        kprintf("%s misaligned address %p for type %x, which requires %d byte alignment\n", type_check_kinds[data->type_check_kind], pointer, data->type, alignment);
+    }
+
     print_location(&data->loc);
+    kprintf("pointer: %p\n", pointer);
+    kprintf("alignment: %u %u\n", data->log_alignment, alignment);
+
     generic_panic("type mismatch");
 }
 
